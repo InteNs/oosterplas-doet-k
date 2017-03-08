f5da47b07ccbc1ddd9e67d8504f5fb3bca92bf93
jordibenck <jordi.b-98@hotmail.com>
2017-03-08 12:34:48 +0100

    added 3 users as seed data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insertGetId([
+            'name' => 'admin',
+            'email' => 'admin@oosterplasdoet.com',
+            'password' => bcrypt('admin1'),
+        ]);
+
+        DB::table('users')->insertGetId([
+            'name' => 'moderator',
+            'email' => 'mod@oosterplasdoet.com',
+            'password' => bcrypt('moderator'),
+        ]);
+
+        DB::table('users')->insertGetId([
+            'name' => 'manager',
+            'email' => 'manager@oosterplasdoet.com',
+            'password' => bcrypt('manager'),
+        ]);
+    }
+}
