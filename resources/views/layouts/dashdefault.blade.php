f4aeaf26086fb5f4ae69f648359fa004770d2fdd
Hannah <jcmaurit@avans.nl>
2017-03-15 11:22:02 +0100

    add dashboard

---
+++ resources/views/layouts/dashdefault.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- Styles -->
+    <!-- CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+
+
+
+
+</head>
+<body>
+@include('layouts.dashheader')
+@include('layouts.dashmenu')
+
+@yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
