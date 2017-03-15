5dde234a1e981c1c3459ab6414db1999857a13ca
jordibenck <jordi.b-98@hotmail.com>
2017-03-15 12:37:20 +0100

    added footer and embedded twitter timeline

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div>
+        <div style="float: left; width: 33%;">
+            <a class="twitter-timeline" data-lang="nl" data-height="300"
+               href="https://twitter.com/OosterplasDoet"
+               data-chrome="nofooter">Tweets by OosterplasDoet</a>
+            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+    </div>
+    <!-- Rainbow Large -->
+    <div class="no-padding">
+        <img class="home-img" src="images/rainbow.png">
+    </div>
+</footer>
