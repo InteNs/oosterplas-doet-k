1f54c63a91eead6f8f054c66e9ed196c3c9cbdfa
Hannah <jcmaurit@avans.nl>
2017-03-17 11:22:16 +0100

    plaatjes aangepast

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,19 +22,19 @@
             <div class="col-sm-12 col-md-6">
                 <div class="thumbnail">
                     <div class="caption">
-                        <h3>Lorum Ipsum</h3>
+                        <h3>Browser usage</h3>
                     </div>
                     <img src="http://img.clickonf5.org/it/5WaysToCreatePieChartsandGraphsOnline_B269/gogle_docs_pie_thumb.gif" alt="...">
                 </div>
             </div>
-            <div class="col-sm-12 col-md-6">
-                <div class="thumbnail">
-                    <div class="caption">
-                        <h3>Lorum Ipsum</h3>
-                    </div>
-                    <img src="https://conceptdraw.com/a2064c3/p1/preview/640/pict--vertical-bar-chart-bar-charts---vector-stencils-library.png--diagram-flowchart-example.png" alt="...">
-                </div>
-            </div>
+            {{--<div class="col-sm-12 col-md-6">--}}
+                {{--<div class="thumbnail">--}}
+                    {{--<div class="caption">--}}
+                        {{--<h3>Lorum Ipsum</h3>--}}
+                    {{--</div>--}}
+                    {{--<img src="https://conceptdraw.com/a2064c3/p1/preview/640/pict--vertical-bar-chart-bar-charts---vector-stencils-library.png--diagram-flowchart-example.png" alt="...">--}}
+                {{--</div>--}}
+            {{--</div>--}}
         </div>
     </div>
 @endsection
