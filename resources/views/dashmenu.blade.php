9c0e57bb5b4a07185a228083529574857f76129c
ypfhpeel <ypfhpeel@avans.nl>
2017-03-29 23:38:38 +0200

    color accents dashboard
    Add 4 default color values to dashboard css. use in menu

--- resources/views/dashmenu.blade.php
+++ resources/views/dashmenu.blade.php
@@ -1,11 +1,20 @@
 
 <div class="sidebar col-md-2 col-sm-12">
     <ul class="nav nav-pills nav-stacked">
-        <li class="smallDevice" role="presentation"><a href="#"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Profiel</a></li>
-        <li class="smallDevice" role="presentation"><a href="#"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Uitloggen</a></li>
-        <li role="presentation"><a href="#"><span class="glyphicon glyphicon-plus left" aria-hidden="true"></span>  Toevoegen</a></li>
-        <li role="presentation"><a href="#"><span class="glyphicon glyphicon-list-alt left" aria-hidden="true"></span> Alle berichten</a></li>
-        <li role="presentation"><a href="#"><span class="glyphicon glyphicon-wrench left" aria-hidden="true"></span>  Instellingen</a></li>
+
+        <!--
+            Voor gebruikerservaring te verhogen gebruik kleuren
+            Plaats de volgende waarde in de class van een <li>
+            - "hover_green"
+            - "hover_orange"
+            - "hover_blue"
+            - "hover_red"
+        -->
+        <li class="smallDevice"  role="presentation"><a href="#"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Profiel</a></li>
+        <li class="smallDevice"  role="presentation"><a href="#"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Uitloggen</a></li>
+        <li role="presentation" class="hover_green"><a href="#"><span class="glyphicon glyphicon-plus left" aria-hidden="true"></span>  Toevoegen</a></li>
+        <li role="presentation" class="hover_blue"><a href="#"><span class="glyphicon glyphicon-list-alt left" aria-hidden="true"></span> Alle berichten</a></li>
+        <li role="presentation" class="hover_red"><a href="#"><span class="glyphicon glyphicon-wrench left" aria-hidden="true"></span>  Instellingen</a></li>
     </ul>
 </div>
 <style>
@@ -92,6 +101,39 @@
             display: none;
         }
     }
+    .hover_red:hover{
+        color: #f2f2f2;
+    }
+    .nav-pills > li.active > .hover_red > a:hover, .nav > .hover_red > a:hover{
+        color : #f2f2f2;
+        background: #DB3912;
+    }
+
+    .nav-pills > li.active > .hover_orange > a:hover, .nav > .hover_orange > a:hover{
+        color : #f2f2f2;
+        background: #FE9900;
+    }
+
+    .nav-pills > li.active > .hover_blue > a:hover, .nav > .hover_blue > a:hover{
+        color : #f2f2f2;
+        background: #4584Ed;
+    }
+
+    .nav-pills > li.active > .hover_green > a:hover, .nav > .hover_green > a:hover{
+        color : #f2f2f2;
+        background: #2ae212;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 </style>
