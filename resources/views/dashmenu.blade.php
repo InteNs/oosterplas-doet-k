aa39a3b643b52c480b6c69c00f6ba8d34fde5f6b
Arno <arnoderks@live.nl>
2017-04-06 19:46:32 +0200

    admin menu fix

--- resources/views/dashmenu.blade.php
+++ resources/views/dashmenu.blade.php
@@ -8,15 +8,44 @@
             - "hover_blue"
             - "hover_red"
         -->
-        <li class="smallDevice" role="presentation"><a href="#"> <span class="glyphicon glyphicon-user"
-                                                                       aria-hidden="true"></span>&nbsp;Profiel</a></li>
-        <li class="smallDevice" role="presentation"><a href="#"><span class="glyphicon glyphicon-log-out"
-                                                                      aria-hidden="true"></span>&nbsp;Uitloggen</a></li>
-        <li role="presentation" class="hover_green"><a href="#"><span class="glyphicon glyphicon-plus left"
-                                                                      aria-hidden="true"></span> Toevoegen</a></li>
-        <li role="presentation" class="hover_blue"><a href="#"><span class="glyphicon glyphicon-list-alt left"
-                                                                     aria-hidden="true"></span> Alle berichten</a></li>
-        <li role="presentation" class="hover_red"><a href="#"><span class="glyphicon glyphicon-wrench left"
-                                                                    aria-hidden="true"></span> Instellingen</a></li>
+        <li role="presentation" class="hover_red">
+            <a href="#">
+                <span class="glyphicon glyphicon-wrench left" aria-hidden="true"></span> Instellingen
+            </a>
+        </li>
+        <li role="presentation" class="hover_green">
+            <a href="/beheer/activiteit">
+                <span class="glyphicon glyphicon-list-alt left" aria-hidden="true"></span> Activiteiten
+            </a>
+            <ul>
+                <li class="hover_blue">
+                    <a href="/beheer/activiteit/create">
+                        <span class="glyphicon glyphicon-plus left" aria-hidden="true"></span> Toevoegen
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li role="presentation" class="hover_green">
+            <a href="/beheer/categorie">
+                <span class="glyphicon glyphicon-tags left" aria-hidden="true"></span> Categorieën
+            </a>
+            <ul>
+                <li class="hover_blue">
+                    <a href="/beheer/categorie/create">
+                        <span class="glyphicon glyphicon-plus left" aria-hidden="true"></span> Toevoegen
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="smallDevice" role="presentation">
+            <a href="#">
+                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Profiel
+            </a>
+        </li>
+        <li class="smallDevice" role="presentation">
+            <a href="#">
+                <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Uitloggen
+            </a>
+        </li>
     </ul>
 </div>
