c2b1d48ec17c0f3921eede1449623fd346b50b59
Mark Havekes <mark.havekes@gmail.com>
2017-04-06 16:05:51 +0200

    improve activity show view

--- resources/views/admin/activity/show.blade.php
+++ resources/views/admin/activity/show.blade.php
@@ -1,15 +1,61 @@
 @extends('layouts.dashdefault')
 
 @section('content')
-    <h1>
-        {{$activity->title}}
-    </h1>
-    {{$activity->description}}<br>
-    Datum: {{$activity->date}}<br>
-    Category ID: {{$activity->category_id}}<br>
-    Prijs: {{$activity->price}}<br>
-    Afbeelding: {{$activity->image}}<br>
-    <img src="{{$activity->image}}">
-    Aangemaakt op: {{$activity->created_at}}<br>
-    Bijgewerkt op: {{$activity->updated_at}}<br>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <h1>{{$activity->title}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="description">Omschrijving:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <h4>{{$activity->description}}</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="description">Categorie:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <h4>{{$activity->category_id}}</h4>
+                {{--TODO SHOW CATEGORIE--}}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="description">Prijs:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <h4 class="glyphicon-euro">{{$activity->price}}</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="image">Afbeelding:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <img src="{{$activity->image}}" class="img-responsive">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="image">Aangemaakt op:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <h4>{{$activity->created_at}}</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h4><label for="image">Gewijzigd op:</label></h4>
+            </div>
+            <div class="col-md-6">
+                <h4>{{$activity->updated_at}}</h4>
+            </div>
+        </div>
+    </div>
 @endsection
