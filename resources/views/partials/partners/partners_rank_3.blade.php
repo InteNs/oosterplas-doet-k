2b4c304e0604c9fe7e809734e680c492a5d44352
ypfhpeel <ypfhpeel@avans.nl>
2017-05-16 21:53:25 +0200

    partners

--- resources/views/partials/partners/partners_rank_3.blade.php
+++ resources/views/partials/partners/partners_rank_3.blade.php
@@ -1,10 +1,10 @@
-<div class=" col-md-4 partial_rang_three">
-    <div class="col-md-12">
+<div class="col-md-4 col-xs-12 partial_rang_three">
+    <div class="col-md-12 col-xs-12" >
         <a href="{{$partnerRank3->link}}">
-            <img class="rang_three_img" src="{{$partnerRank3->image}}" />
+            <img class="rang_three_img" src="{{($partnerRank3->image != '') ? $partnerRank3->image : 'http://english.tw/wp-content/themes/qaengine/img/default-thumbnail.jpg'}}" />
         </a>
     </div>
-    <div class="col-md-12">
+    <div class="col-md-12 col-xs-12">
         <h4>
             {{$partnerRank3->name}}
         </h4>
