65882694717ae967a6192a8b5a6f92f9fd32ca58
Hannah Mauritz <hajcmaurit@avans.nl>
2017-05-30 12:09:56 +0200

    twitterfeed verwijderd

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,6 @@
 <footer class="container-fluid">
     <div class="address">
         <adress>
-
             <br>
             <strong>Stichting OosterplasDOET!<br></strong>
             <div class="infolist">
@@ -13,7 +12,6 @@
                     <p translate="no" itemscope itemtype="http://schema.org/PostalAddress">
                         <span itemprop="streetAddress"> {{ $settings->address }} </span><br>
                         <span itemprop="postalCode"> {{ $settings->zipcode }} </span>
-
                     </p>
                 </dd>
             </div>
@@ -23,20 +21,5 @@
             <strong><br>Emailadres:</strong><br>{{ $settings->email }}<br>
         </adress>
     </div>
-
-
-        <div class="twitter col-md-4">
-            <a class="twitter-timeline" data-lang="nl" data-height="500"
-               href="https://twitter.com/OosterplasDoet"
-               data-chrome="nofooter">Tweets by OosterplasDoet</a>
-            <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
-
-        <div class="facebook col-md-4">
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Foosterplasdoet&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
-                height="500"  style="border:none;overflow:hidden;width:100%" scrolling="no" frameborder="0" allowTransparency="true">
-        </iframe>
-        </div>
-        <br>
-
+        <a href="mailto:someone@example.com"></a>
 </footer>
